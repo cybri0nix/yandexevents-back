c7807cfb2f5d475dc2316577c5cc5f6b551bf422
cybri0nix <cybri0nix19@gmail.com>
2017-09-13 15:00:57 +0300

    event.is_left flag added

--- api/api.js
+++ api/api.js
@@ -1,4 +1,5 @@
 const fakeDB = require('./fakeDB.js')
+const m = require('moment')
 
 /**
  * Реальные данные, но вшитые статично. см. ./mocks
@@ -19,6 +20,7 @@ module.exports = {
   getPlace: getPlace,
   getEntities: getEntities,
   getHint: getHint,
+  getEntity: getEntity,
 }
 
 function getHolidays(req, res, next) {
@@ -43,7 +45,35 @@ function getEvents(req, res, next) {
   params['orderMode'] = req.query.order_mode
   params['entity'] = req.query.entity
 
-  const data = fakeDB.getEvents(params)
+  let data = fakeDB.getEvents(params)
+  // for test
+  // let data = [
+  //   {end_time: '2017-08-09'},
+  //   {end_time: '2017-09-09 00:09'},
+  //   {end_time: '2017-09-09 23:59'}
+  // ]
+  const todayMoment = m().utc()
+  const today = todayMoment.unix()
+  const yesterday = todayMoment.add(-1, 'days')
+
+  data.length && data.map((item) => {
+    const end_dt = m(item.end_time).utc().unix()
+    
+    // Is left?
+    item.is_left = true // all is left
+    // if (today > end_dt) {
+    //   item.is_left = true
+    // }
+    item.photo_small = `http://io.yamblz.ru/i/events/${item.id}_small.jpg`
+    item.photo_large = `http://io.yamblz.ru/i/events/${item.id}_large.jpg`
+
+    return item
+  })
+
+  // is yesterday event then hide?
+  // data = data.filter((item) => {
+  //   return yesterday.unix() < m(item.end_time).utc().unix()
+  // })
   _responseSuccess(res, data)
 }
 
@@ -59,6 +89,18 @@ function getEvent(req, res, next) {
     })
   }
   else {
+    const today = m().utc().unix()
+    const end_dt = m(data[0].end_time).utc().unix()
+    
+    // Is left?
+    data[0].is_left = false
+    if (today > end_dt) {
+      data[0].is_left = true
+    }
+
+    data[0].photo_small = `http://io.yamblz.ru/i/events/${data[0].id}_small.jpg`
+    data[0].photo_large = `http://io.yamblz.ru/i/events/${data[0].id}_large.jpg`
+
     _responseSuccess(res, data[0])
   }
 }
@@ -106,6 +148,22 @@ function getEntities(req, res, next) {
   const category = req.query.category
   data = fakeDB.getEntities(holiId, category)
   _responseSuccess(res, data)
+}
+
+function getEntity(req, res, next) {
+  const holiId = ~~req.query.holiday
+  const entityId = req.query.id
+  const data = fakeDB.getEntity(entityId, holiId)
+
+  if (!data.length) {
+    _responseError(res, {
+      "msg": "Entity not found",
+      "code": ERROR_NOT_FOUND
+    })
+  }
+  else {
+    _responseSuccess(res, data[0])
+  }
 }
 
 function getHint(req, res, next) {
